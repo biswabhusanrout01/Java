368c99c68862caf125d662a8b20e09f35ac196bb
biswabhusanrout01 <routbiswabhusan0621@gmail.com>
2025-03-14 07:57:21 +0000

    Added operators in Java

---
+++ operator in java/add.java
@@ -0,0 +1,11 @@
+public class add {
+   public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 10;
+    int add = num1 + num2;
+
+    System.out.println(num1 +"+"+ num2 +"="+ add );
+    
+
+   }
+}
---
+++ operator in java/multiply.java
@@ -0,0 +1,11 @@
+
+
+public class multiply {
+   public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 6;  
+    System.out.println( num1 * num2 );
+
+
+   }
+}
